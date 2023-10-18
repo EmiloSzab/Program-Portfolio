6fe703ff84a910fb2944f66772c377fcff2295f7
EmiloSzab <szabemil@gmail.com>
2023-10-18 13:56:56 +0100

    Tasks

---
+++ programs01.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+
+    print('Hello Emil')
+
+    celsius = int(input('What temperature is it? '))
+
+    fahrenheit = (celsius * 1.8) + 32
+
+    print (f'In York, it\'s {celsius}C and {fahrenheit}F ')
+
